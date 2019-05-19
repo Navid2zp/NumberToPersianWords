de3f02c0193b0358810b2d7a52e2acca0305f011
navid <navid2zp@gmail.com>
2019-05-19 18:22:39 +0430

    naming changes

--- run_test.go
+++ run_test.go
@@ -5,12 +5,12 @@ import (
 )
 
 func TestMax(t *testing.T) {
-	num := Parse(9223372036854775807)
+	num := ParseInt(9223372036854775807)
 	println(num)
 }
 
 func BenchmarkMax(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		Parse(9223372036854775807)
+		ParseInt(9223372036854775807)
 	}
 }
